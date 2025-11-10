fb4c06119e04c6599c1db7b0f15652d62afa7acf
aNOOBisTheGod <yablokoibanan@gmail.com>
2025-11-10 21:29:36 +0300

    fix before CI/CD docker setup

--- lib/src/domain/models/note.g.dart
+++ lib/src/domain/models/note.g.dart
@@ -18,18 +18,6 @@ Note _$NoteFromJson(Map<String, dynamic> json) => Note(
       lastUpdatedBy: json['last_updated_by'] as String,
     );
 
-Map<String, dynamic> _$NoteToJson(Note instance) => <String, dynamic>{
-      'id': instance.id,
-      'text': instance.text,
-      'importance': _$NoteStatusEnumMap[instance.status]!,
-      'deadline': instance.deadline,
-      'done': instance.isDone,
-      'color': instance.color,
-      'created_at': instance.createdAt,
-      'changed_at': instance.changedAt,
-      'last_updated_by': instance.lastUpdatedBy,
-    };
-
 const _$NoteStatusEnumMap = {
   NoteStatus.high: 'important',
   NoteStatus.low: 'low',
